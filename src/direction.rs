90b5f7e804b435a3cb251396425e52afdd3ffab9
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-06 17:24:49 +0200

    direction: add unit test

--- src/direction.rs
+++ src/direction.rs
@@ -75,3 +75,41 @@ impl Drop for Direction {
         self.motor_two_enable_pin.set_low();
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use rppal::gpio::{Error, Pin};
+
+    fn generate_test_direction() -> Direction {
+        let motor_one_pin: MotorPin = MotorPin {
+            enable: 5,
+            input_one: 6,
+            input_two: 13,
+        };
+        let motor_two_pin: MotorPin = MotorPin {
+            enable: 10,
+            input_one: 9,
+            input_two: 11,
+        };
+        Direction::build(motor_one_pin, motor_two_pin).unwrap()
+    }
+
+    #[test]
+    fn build_should_create_direction() -> () {
+        let direction_buffer: Direction = generate_test_direction();
+        let motor_one_enable_pin: Result<Pin, Error> = Gpio::new().unwrap().get(5);
+        let motor_one_input_one_pin: Result<Pin, Error> = Gpio::new().unwrap().get(6);
+        let motor_one_input_two_pin: Result<Pin, Error> = Gpio::new().unwrap().get(13);
+        let motor_two_enable_pin: Result<Pin, Error> = Gpio::new().unwrap().get(10);
+        let motor_two_input_one_pin: Result<Pin, Error> = Gpio::new().unwrap().get(9);
+        let motor_two_input_two_pin: Result<Pin, Error> = Gpio::new().unwrap().get(11);
+        assert!(motor_one_enable_pin.is_err());
+        assert!(motor_one_input_one_pin.is_err());
+        assert!(motor_one_input_two_pin.is_err());
+        assert!(motor_two_enable_pin.is_err());
+        assert!(motor_two_input_one_pin.is_err());
+        assert!(motor_two_input_two_pin.is_err());
+    }
+}
