2aa2ebfded62e86bd1bb028e74d0ac2d64a0ff0d
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-06 16:17:56 +0200

    direction: test real

--- src/direction.rs
+++ src/direction.rs
@@ -1,6 +1,5 @@
 use crate::config::MotorPin;
 use rppal::gpio::{Gpio, OutputPin};
-use rppal::pwm::{Channel, Polarity, Pwm};
 use std::error::Error;
 
 pub struct Direction {
@@ -10,8 +9,6 @@ pub struct Direction {
     motor_two_enable_pin: OutputPin,
     motor_two_input_one_pin: OutputPin,
     motor_two_input_two_pin: OutputPin,
-    pub pwn_one: Pwm,
-    pub pwn_two: Pwm,
 }
 
 impl Direction {
@@ -26,9 +23,13 @@ impl Direction {
             motor_two_enable_pin: Gpio::new()?.get(motor_two_pin.enable)?.into_output(),
             motor_two_input_one_pin: Gpio::new()?.get(motor_two_pin.input_one)?.into_output(),
             motor_two_input_two_pin: Gpio::new()?.get(motor_two_pin.input_two)?.into_output(),
-            pwn_one: Pwm::with_frequency(Channel::Pwm0, 8000.0, 1.0, Polarity::Normal, true)?,
-            pwn_two: Pwm::with_frequency(Channel::Pwm1, 8000.0, 1.0, Polarity::Normal, true)?,
         })
+    }
+
+    pub fn init(&mut self) -> () {
+        self.motor_one_enable_pin.set_high();
+        self.motor_two_enable_pin.set_high();
+        self.stop();
     }
 
     pub fn forward(&mut self) -> () {
@@ -64,5 +65,14 @@ impl Direction {
         self.motor_one_input_two_pin.set_low();
         self.motor_two_input_one_pin.set_low();
         self.motor_two_input_two_pin.set_low();
+    }
+}
+
+impl Drop for Direction {
+    fn drop(&mut self) -> () {
+        self.stop();
+        self.motor_one_enable_pin.set_low();
+        self.motor_two_enable_pin.set_low();
+        println!("tmp drop dir")
     }
 }
