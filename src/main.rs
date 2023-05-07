407433f6e13caaacc50b5377d90ba1ba8cddfbec
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-07 22:12:27 +0200

    config: add test real ultrasonic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use alpyne_rust::config;
 use alpyne_rust::config::Config;
 use alpyne_rust::direction::Direction;
+use alpyne_rust::sense::ultrasonic::UltrasonicSensor;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     let config: Config = Config::read("config.toml");
-    let mut direction: Direction =
+    /*let mut direction: Direction =
         Direction::build(config.motor_one_pin, config.motor_two_pin).unwrap();
     direction.init();
 
@@ -21,5 +23,14 @@ fn main() {
     direction.left();
     thread::sleep(Duration::from_millis(2000));
 
-    direction.stop();
+    direction.stop();*/
+    let t = UltrasonicSensor(config::UltrasonicPin);
+    println!("{}", t.get_distance());
+    thread::sleep(Duration::from_millis(2000));
+    println!("{}", t.get_distance());
+    thread::sleep(Duration::from_millis(2000));
+    println!("{}", t.get_distance());
+    thread::sleep(Duration::from_millis(2000));
+    println!("{}", t.get_distance());
+    thread::sleep(Duration::from_millis(2000));
 }
