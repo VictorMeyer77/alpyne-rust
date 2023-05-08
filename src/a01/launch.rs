158c9b3311eb913c803a261bbf6fb40492163614
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-08 17:05:33 +0200

    a01: change block detection

--- src/a01/launch.rs
+++ src/a01/launch.rs
@@ -44,9 +44,15 @@ fn a01_loop(
 }
 
 fn is_blocked(history_dist: &Vec<u16>, history_max_size: usize) -> bool {
-    let mut history_dist_buffer: Vec<u16> = history_dist.clone();
-    history_dist_buffer.dedup();
-    history_dist_buffer.len() == 1 && history_dist.len() == history_max_size
+    let low_dist: usize = history_dist.iter().filter(|x| **x < 10).count();
+    let high_dist: usize = history_dist.iter().filter(|x| **x > 1000).count();
+    let history_max = history_dist.iter().max();
+    let history_min = history_dist.iter().min();
+    let is_blocked_a: bool = history_max.is_some()
+        && history_min.is_some()
+        && (history_max.unwrap() - history_min.unwrap() < 5);
+    let is_blocked_b: bool = low_dist > 0 && high_dist > 0;
+    history_dist.len() == history_max_size && (is_blocked_a || is_blocked_b)
 }
 
 fn history_add(history_dist: &mut Vec<u16>, history_max_size: usize, value: u16) -> () {
@@ -64,9 +70,13 @@ mod tests {
 
     #[test]
     fn is_blocked_should_return_true_when_history_distance_is_same() -> () {
-        let blocked_history: Vec<u16> = vec![1, 1, 1, 1, 1];
-        let normal_history: Vec<u16> = vec![1, 2, 3, 4, 5];
-        assert!(is_blocked(&blocked_history, 5));
+        let blocked_history_a: Vec<u16> = vec![1, 1, 1, 1, 1];
+        let blocked_history_b: Vec<u16> = vec![1, 2, 1, 3, 1];
+        let blocked_history_c: Vec<u16> = vec![5, 1200, 6, 1200, 8];
+        let normal_history: Vec<u16> = vec![10, 21, 11, 24, 35];
+        assert!(is_blocked(&blocked_history_a, 5));
+        assert!(is_blocked(&blocked_history_b, 5));
+        assert!(is_blocked(&blocked_history_c, 5));
         assert!(!is_blocked(&normal_history, 5));
     }
 
